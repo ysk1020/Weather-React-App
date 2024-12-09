86174fef2aa401a4854739a469bc7c34adbadc81
Yussuf <ggo353410@gmail.com>
2024-12-10 00:55:06 +0200

    icon, and search bar v2

--- src/Components/Weather_icon.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from "react";
-
-function WeatherIcon() {
-  const [weather, setWeather] = useState(null);
-  const weatherIcons = {};
-  return <></>;
-}
-
-export default WeatherIcon;
